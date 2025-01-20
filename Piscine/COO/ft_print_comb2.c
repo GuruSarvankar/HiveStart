c343b48e6b9589dfaace7eb9e180819bfcb1a952
gururefresh <gururefresh@gmail.com>
2025-01-20 11:12:36 +0200

    Create a function that displays different combination of two two digits (xx xx) between 00 and 99, listed by ascending order

---
+++ Piscine/COO/ft_print_comb2.c
@@ -0,0 +1,39 @@
+#include<unistd.h>
+
+void ft_print_comb2(void){
+
+    char first[2];
+    char second[2];
+
+    first[0] = '0';
+    while (first[0] <= '9')
+    {
+        first[1]='0';
+        while (first[1] <= '9')
+        {
+            second[0] = first[0];        
+            while (second[0] <= '9')
+            {
+                second[1] = first[1] + 1;
+                while (second[1]<='9')
+                {
+                    write(1, &first[0], 1 );
+                    write(1, &first[1], 1 );
+                    write(1, "-", 1 );
+                    write(1, &second[0], 1);
+                    write(1, &second[1], 1);
+                    write(1, ", ", 2 );
+                    second[1]++;
+                }
+                second[0]++;
+            }
+             first[1]++;
+        }
+        first[0]++;
+    }  
+}
+
+int main(){
+    ft_print_comb2();
+    return 0;
+}
